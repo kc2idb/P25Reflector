8ebb1ed3a64bb6a07a0d66bd2066ddde0f4afb40
kc2idb <ken@kc2idb.net>
2023-08-03 22:02:50 -0400

    added tz data and updated refresh rate for dashboard

--- P25Dash/config/config.php
+++ P25Dash/config/config.php
@@ -2,7 +2,7 @@
 # This is an auto-generated config-file!
 # Be careful, when manual editing this!
 
-date_default_timezone_set('UTC');
+date_default_timezone_set('America/New_York');
 define("P25REFLECTORLOGPATH", "/data");
 define("P25REFLECTORLOGPREFIX", "P25Reflector");
 define("P25REFLECTORINIPATH", "/data");
@@ -10,7 +10,7 @@ define("P25REFLECTORINIFILENAME", "P25Reflector.ini");
 define("P25REFLECTORPATH", "/data");
 define("TIMEZONE", "America/New_York");
 define("LOGO", "DVSwitch.png");
-define("REFRESHAFTER", "60");
+define("REFRESHAFTER", "5");
 define("TEMPERATUREHIGHLEVEL", "60");
 define("SHOWQRZ", "");
 ?>
